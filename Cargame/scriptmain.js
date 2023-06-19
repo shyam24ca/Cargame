81a0e00263e30e2e73985b10340b08733452a82c
Shyam Enumula <shyamenumula@Shyams-MBP.lan>
2023-06-18 21:30:03 -0400

    UpdatedCode

---
+++ Cargame/scriptmain.js
@@ -0,0 +1,41 @@
+var blueCar = document.getElementById("bluecar");
+var redCar = document.getElementById("redcar");
+var result = document.getElementById("result");
+var score = document.getElementById("score");
+var game = document.getElementById("game");
+
+var counter = 0;
+
+blueCar.addEventListener("animationiteration", function(){
+  var random = ((Math.floor(Math.random()*3))*100)
+  blueCar.style.left = random+ "px";
+  counter++;
+})
+
+window.addEventListener("keydown",function(e){
+  if(e.keyCode == "39"){
+    var redCarleft = parseInt(window.getComputedStyle(redCar).getPropertyValue("left"))
+    if(redCarleft<200){
+      redCar.style.left = (redCarleft+100)+"px";
+  };
+  
+  if(e.keyCode == "37"){
+    var redCarleft = parseInt(window.getComputedStyle(redCar).getPropertyValue("left"))
+    if(redCarleft>0){
+      redCar.style.left = (redCarleft-100)+"px";
+    }
+  }
+  }
+})
+
+setInterval(function GameOver(){
+  var blueCarTop = parseInt(window.getComputedStyle(blueCar).getPropertyValue("top"))
+  var blueCarLeft = parseInt(window.getComputedStyle(blueCar).getPropertyValue("left"))
+  var redCarLeft = parseInt(window.getComputedStyle(redCar).getPropertyValue("left"))
+  if((blueCarLeft === redCarLeft)&&(blueCarTop>250)&&(blueCarTop<450)){
+    result.style.display = "block";
+    game.style.display = "none";
+    score.innerHTML = `Your score is ${counter}`;
+    counter = 0;
+  }
+}, 10)
